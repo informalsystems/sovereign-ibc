08e70c642fb28723fc9b07c2935fd911f5d4d664
Philippe Laferriere <plafer@protonmail.com>
2023-09-12 15:41:04 -0400

    more comments

--- module-system/module-implementations/sov-ibc/src/call.rs
+++ module-system/module-implementations/sov-ibc/src/call.rs
@@ -24,6 +24,9 @@ pub enum CallMessage<C: sov_modules_api::Context> {
     // Q: Should this be `Core(Any)` instead, so that we don't expose our `MsgEnvelope` to the rpc interface?
     // In that case though, the bytes in `Any` would probably be encoded with `hex`, which imposes big parsing
     // costs to the node.
+    //
+    // Or maybe we use a type local to this module that is very similar to `MsgEnvelope` and use that,
+    // to enable ibc-rs to change `MsgEnvelope` in the future?
     Core(MsgEnvelope),
 
     Transfer(SDKTokenTransfer<C>),
