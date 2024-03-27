97aacfb514bfb05638d4c042eb20ad9b38a73624
Farhad Shabani <Farhad.Shabani@gmail.com>
2024-03-27 11:47:24 -0700

    fix: obtain `next_validator_hash` of `CelestiaHeader` using Protobuf decoding (#110)
    * fix: obtain next_validator_hash through Probobuf decoding

--- modules/sov-consensus-state-tracker/src/celestia_da.rs
+++ modules/sov-consensus-state-tracker/src/celestia_da.rs
@@ -1,4 +1,5 @@
 use ibc_core::commitment_types::commitment::CommitmentRoot;
+use ibc_core::primitives::proto::Protobuf;
 pub use sov_celestia_adapter::verifier::CelestiaSpec;
 pub use sov_celestia_adapter::*;
 use sov_celestia_client::consensus_state::ConsensusState as HostConsensusState;
@@ -15,11 +16,10 @@ impl HasConsensusState for CelestiaSpec {
             header.time().secs(),
             header.time().subsec_nanos(),
         )
-        .expect("time is valid");
+        .expect("Could not obtain timestamp from header");
 
-        let next_validator_hash =
-            tendermint::Hash::try_from(header.header.next_validators_hash.clone())
-                .expect("hash is valid");
+        let next_validator_hash = tendermint::Hash::decode_vec(&header.header.next_validators_hash)
+            .expect("Could not decode next validator hash from header");
 
         SovConsensusState {
             root: CommitmentRoot::from_bytes(header.hash().as_ref()),
