cd126fedf101d643a7484897efa39e0b7da90aca
Ranadeep Biswas <mail@rnbguy.at>
2024-04-08 12:23:52 +0200

    traits to retrieve value with or without proofs

--- modules/sov-ibc/src/rpc/helpers.rs
+++ modules/sov-ibc/src/rpc/helpers.rs
@@ -1,11 +1,14 @@
+use core::marker::PhantomData;
 use std::cell::RefCell;
 use std::rc::Rc;
 
+use borsh::BorshSerialize;
 use ibc_core::client::types::Height;
 use ibc_core::host::ValidationContext;
 use jsonrpsee::core::RpcResult;
 use jsonrpsee::types::ErrorObjectOwned;
-use sov_modules_api::{Spec, WorkingSet};
+use sov_modules_api::{Spec, StateMap, WorkingSet};
+use sov_state::storage::{StateCodec, StateItemCodec};
 
 use crate::context::IbcContext;
 use crate::Ibc;
@@ -39,4 +42,68 @@ pub fn to_jsonrpsee_error(err: impl ToString) -> ErrorObjectOwned {
         err.to_string(),
         None::<String>,
     )
+}
+
+/// Trait for proof agnostic storage value retrieval.
+pub trait StorageValue<V> {
+    type Output;
+    fn value_at_key<K, C, S>(
+        key: &K,
+        store: &StateMap<K, V, C>,
+        working_set: &mut WorkingSet<S>,
+    ) -> Self::Output
+    where
+        S: Spec,
+        C: StateCodec,
+        <C as StateCodec>::ValueCodec: StateItemCodec<V>,
+        <C as StateCodec>::KeyCodec: StateItemCodec<K>;
+}
+
+/// Implementation of `StorageValue` for values without proofs.
+pub struct WithoutProof<V>(PhantomData<V>);
+
+impl<V> StorageValue<V> for WithoutProof<V> {
+    type Output = Option<V>;
+
+    fn value_at_key<K, C, S>(
+        key: &K,
+        store: &StateMap<K, V, C>,
+        working_set: &mut WorkingSet<S>,
+    ) -> Self::Output
+    where
+        S: Spec,
+        C: StateCodec,
+        <C as StateCodec>::ValueCodec: StateItemCodec<V>,
+        <C as StateCodec>::KeyCodec: StateItemCodec<K>,
+    {
+        store.get(key, working_set)
+    }
+}
+
+/// Implementation of `StorageValue` for values with proofs.
+pub struct WithProof<V>(PhantomData<V>);
+
+impl<V> StorageValue<V> for WithProof<V> {
+    type Output = (Option<V>, Vec<u8>);
+
+    fn value_at_key<K, C, S>(
+        key: &K,
+        store: &StateMap<K, V, C>,
+        working_set: &mut WorkingSet<S>,
+    ) -> Self::Output
+    where
+        S: Spec,
+        C: StateCodec,
+        <C as StateCodec>::ValueCodec: StateItemCodec<V>,
+        <C as StateCodec>::KeyCodec: StateItemCodec<K>,
+    {
+        let result = store.get_with_proof(key, working_set);
+
+        (
+            result
+                .value
+                .map(|bytes| store.codec().value_codec().decode_unwrap(bytes.value())),
+            result.proof.try_to_vec().expect("no error"),
+        )
+    }
 }
