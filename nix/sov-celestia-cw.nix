88af9ef92e699cfc3561b11ffb9ff314fc32b2f9
Ranadeep Biswas <mail@rnbguy.at>
2024-06-05 23:32:56 +0200

    update lockfiles

--- nix/sov-celestia-cw.nix
+++ nix/sov-celestia-cw.nix
@@ -67,15 +67,15 @@ let
         cargoLock = {
             lockFile = cargo-lock-file;
             outputHashes = {
-                "basecoin-0.1.0" = "sha256-wLG6PWpd8HwM/pquZGFAIluEPUl7628rxTsTO7fztAM=";
+                "basecoin-0.1.0" = "sha256-q5dHiYeQG8DTO9grL196MH0hFYjIoec9U0roiGozzDE=";
                 "celestia-proto-0.1.0" = "sha256-iUgrctxdJUyhfrEQ0zoVj5AKIqgj/jQVNli5/K2nxK0=";
-                "ibc-0.53.0" = "sha256-8bCgAUoBHxy7GPi8SwSxNJdtFWkMfyHgpZiulgtkcFo=";
+                "ibc-0.53.0" = "sha256-AnRsdl3njOCh0o44GU5uDvX4caxcEitH2Ic3sRu/uIs=";
                 "jmt-0.9.0" = "sha256-pq1v6FXS//6Dh+fdysQIVp+RVLHdXrW5aDx3263O1rs=";
                 "nmt-rs-0.1.0" = "sha256-jcHbqyIKk8ZDDjSz+ot5YDxROOnrpM4TRmNFVfNniwU=";
                 "risc0-cycle-utils-0.3.0" = "sha256-5dA62v1eqfyZBny4s3YlC2Tty7Yfd/OAVGfTlLBgypk=";
                 "rockbound-1.0.0" = "sha256-aDrNegRfsSwiNw4XLsE4rpUYDZn2N59UJbGZ6mpY180=";
                 "tendermint-0.32.0" = "sha256-FtY7a+hBvQryATrs3mykCWFRe8ABTT6cuf5oh9IBElQ=";
-                "cosmos-sdk-proto-0.22.0-pre" = "sha256-TMHdqv3va4Y87P6RN1dEXH6/C280NDr5md2vSHWqsQg=";
+                "cosmos-sdk-proto-0.22.0-pre" = "sha256-r7zoHmk12iTdsyJ/4rRgD5HDeDSiNcs/HlS7eGi+NbE=";
             };
         };
 
